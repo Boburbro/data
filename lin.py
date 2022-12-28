4e0e90f2d7d08fc98e08659f000f95b184e287a0
Boburbro <110478544+Boburbro@users.noreply.github.com>
2022-12-28 20:41:59 +0500

    Update lin.py

--- lin.py
+++ lin.py
@@ -17,13 +17,15 @@ def mainlin(a):
             v = file.write(c)
             os.system(clear)
     if a == 2:
-        os.system("apt-get install python2")
+        os.system("sudo apt-get install python2")
         os.system("cd data")
         os.system("python2 Bobur1c.py")
 
 def li(*a):
-    print("Linux\n[1] TGddos\n[2] DDoS\n[99] Restore factory settings")
+    print("Linux\n[1] TGddos\n[2] DDoS\n[99] Restore factory settings\n[0] exit")
     a = input("\n=>")
+    if a == 0 or a == "0":
+        exit()
     try:
         a = int(a)
     except:
