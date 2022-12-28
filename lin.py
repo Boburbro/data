dd3779a5e16757fdcf75e71f08f7567079c05e82
Boburbro <110478544+Boburbro@users.noreply.github.com>
2022-12-28 21:14:23 +0500

    1

--- lin.py
+++ lin.py
@@ -19,14 +19,15 @@ def mainlin(a):
     if a == 2:
         os.system("sudo apt-get install python2")
         os.system("python2 data/Bobur1c.py")
-
+    if a == 3:
+        os.system("bash data/Bobur2c")
 def li(*a):
     p = input()
     if p == "y":
         os.system("pip install requests")
         os.system("pip install smtp")
         os.system("pip install telethon")
-    print("Linux\n[1] TGddos\n[2] DDoS\n[99] Restore factory settings\n[0] exit")
+    print("Linux\n[1] TGddos\n[2] DDoS\n[3] Admin hack \n[99] Restore factory settings\n[0] exit")
     a = input("\n=>")
     if a == 0 or a == "0":
         exit()
